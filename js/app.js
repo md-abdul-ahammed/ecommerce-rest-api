d74baf6beea81b6c9e6e5825ee7f49c2fd3f74e1
md-abdul-ahammed <shagarmd2082@gmail.com>
2021-09-13 23:23:24 +0600

    Alhamdulillah-All-Done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//Get Api Data
 const loadProducts = () => {
   const url = `https://fakestoreapi.com/products`;
   fetch(url)
@@ -10,34 +11,64 @@ loadProducts();
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
-      <div>
+    div.innerHTML = `<div class="single-product borders">
+      <div class="image-center">
     <img class="product-image" src=${image}></img>
       </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <h3 class="fs-5 fw-bold text-primary">${product.title}</h3>
+      <p><span class="fw-bold">Category:</span> ${product.category}</p>
+      <h2 class="fs-5 fw-bold">Price: <span class="text-danger">$ ${product.price}</span></h2>
+      <p><b>rating: <small>${product?.rating?.rate}/5 (${product?.rating?.count})</small></b></p>
+      <div>
+      <button onclick="addToCart(${product.price})" id="addToCart-btn" class="buy-now btn btn-primary">Add to cart</button>
+      <button onclick="showDetails(${product.id})" class="btn btn-danger">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
 };
+// Calculation Cart Option
 let count = 0;
-const addToCart = (id, price) => {
+const addToCart = (price) => {
   count = count + 1;
   updatePrice("price", price);
 
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
+  updateTotal();
 };
-
+// Single Product Details Api here
+const showDetails = (id) => {
+  fetch(`https://fakestoreapi.com/products/${id}`)
+    .then(res => res.json())
+    .then(data => showSingleProductsDetails(data));
+}
+// Single Product Details UI
+const showSingleProductsDetails = (data) => {
+  const div = document.getElementById('show-details');
+  div.style.display = "block";
+  div.innerHTML = `
+  <div class="row g-0">
+            <div class="col-md-4">
+              <img src="${data.image}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title text-primary fw-bold">${data.title}</h5>
+                <p class="card-text fs-6">${data.description}</p>
+                <h2 class="fs-5 fw-bold">Price: <span class="text-danger">$ ${data.price}</span></h2>
+                <button onclick="addToCart(${data.price})" id="addToCart-btn" class="buy-now btn btn-primary">Add to cart</button>
+              </div>
+            </div>
+          </div>
+  `
+}
+// Convert Price
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -46,14 +77,17 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = decimalValue(total);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = decimalValue(value);
 };
-
+// For Convert Decimal 2 digit
+function decimalValue(x) {
+  return Number.parseFloat(x).toFixed(2);
+}
 // update delivery charge and total Tax
 const updateTaxAndCharge = () => {
   const priceConverted = getInputValue("price");
@@ -70,11 +104,11 @@ const updateTaxAndCharge = () => {
     setInnerText("total-tax", priceConverted * 0.4);
   }
 };
-
 //grandTotal update function
 const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  document.getElementById("total").innerText = decimalValue(grandTotal);
 };
+
